8cbe88d5f481b93fcd7d2d9f5cccea1e6068c8cf
MISU01 <subrata.mitra@sms-group.com>
2020-08-29 12:27:28 +0530

    add user

--- app/app.py
+++ app/app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask import Flask, jsonify, request
 from flask import Flask, request, Response
+from flask_bcrypt import Bcrypt
 from flask.json import dumps
+from flask_bcrypt import Bcrypt
 from database.db import initialize_db
 from database.models import Chemical
 from flask_marshmallow import Marshmallow
 from numpy.f2py.crackfortran import debug, true_intent_list
+from database.models import User
 
 app = Flask(__name__)
 
@@ -40,6 +43,7 @@ app.config['MONGODB_SETTINGS'] = {
 
 db = initialize_db(app)
 ma = Marshmallow(app)
+bcrypt = Bcrypt(app)
 
 class ChemicalSchema(ma.Schema):  
         
@@ -47,9 +51,12 @@ class ChemicalSchema(ma.Schema):
         # Fields to expose
         fields = ("ID", "Name")
 
+chemical_schema = ChemicalSchema()
+chemicals_schema = ChemicalSchema(many=True)
 
-user_schema = ChemicalSchema()
-users_schema = ChemicalSchema(many=True)
+
+
+
 
 @app.route('/')
 def hello():
@@ -58,6 +65,18 @@ def hello():
 @app.route('/chemicals', methods=['GET'])
 def get_chemicals():
     chemicals = Chemical.objects().all()   
-    return Response(users_schema.dumps(chemicals))
+    return Response(chemical_schema.dumps(chemicals))
+
+@app.route('/createuser', methods=['POST'])
+def createuser():
+    body = request.get_json(force=True)
+    user = User(**body)
+    user.hash_password()
+    user.save()
+    id = user.id 
+    return {'id': str(id)}, 200
+  
+
+
 
 app.run(debug = True)
--- app/database/models.py
+++ app/database/models.py
@@ -1,5 +1,17 @@
 from .db import db
+from flask_bcrypt import generate_password_hash, check_password_hash
 
 class Chemical(db.Document):
     ID = db.StringField(required=True, unique=True)
     Name = db.StringField(required=True, unique=True)
+    
+class User(db.Document):
+     email = db.EmailField(required=True, unique=True)
+     password = db.StringField(required=True, min_length=6)
+     
+     def hash_password(self):
+         self.password = generate_password_hash(self.password).decode('utf8')
+       
+     def check_password(self, password):
+         return check_password_hash(self.password, password)
+    
